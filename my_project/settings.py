f491b8a42b9af46e5b96de3e4ef84e5cac31d2bf
Navya Keelara <navyakn31@gmail.com>
2025-01-21 21:35:05 +0000

    Change to DEBUG from false to true

--- my_project/settings.py
+++ my_project/settings.py
@@ -27,6 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['8000-navyakn24-djangoproject-tyqhke5sokm.ws.codeinstitute-ide.net', '.herokuapp.com']
 
+INTERNAL_IPS = [
+    '127.0.0.1',
+    'localhost',
+]
 
 # Application definition
 
@@ -39,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'hello_world',
     'about',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'my_project.urls'
